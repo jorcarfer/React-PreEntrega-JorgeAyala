4c905f601ec4111223776c27c60a9406640f5022
jorcarfer <jorcarfer2@hotmail.com>
2025-10-15 14:26:37 -0300

    Comentarios

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -4,12 +4,11 @@ import Carrito from '../components/Carrito';
 
 const Inicio = () => {
   const [carrito, setCarrito] = useState([]);
-  // Usamos el spread operator (...) para copiar el array existente 
-  // y añadir el nuevo producto.
+
   const agregarAlCarrito = (producto) => {
     setCarrito([...carrito, producto]);
   };
-  // Usamos filter() para crear un nuevo array que excluye el elemento con el índice dado.
+ 
   const eliminarDelCarrito = (indiceAEliminar) => {
     setCarrito(carrito.filter((_, indice) => indice !== indiceAEliminar));
   };
